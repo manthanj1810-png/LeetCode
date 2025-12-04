3502dbdb45843c8cdaa763a1e349acc35b6a6632
Manthan Jain <manthanj1810@gmail.com>
2025-12-04 12:47:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.2 MB (81.49%)

---
+++ 0792-binary-search/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int n=size(nums);
+       int st=0,end=n-1;
+       
+       while(st<=end){
+        int mid=st + (end-st)/2;
+        if(nums[mid]==target) return mid;
+        else if(target>nums[mid]) st=mid+1;
+        else end=mid-1;
+        }
+        return -1;
+    }
+       
+};
