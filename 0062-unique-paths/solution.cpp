6738bb9bd3999176c4d772fa03594ca9a243e81e
Manthan Jain <manthanj1810@gmail.com>
2026-03-02 13:33:45 +0000

    Sync LeetCode submission Runtime - 2 ms (20.94%), Memory - 9.5 MB (30.18%)

---
+++ 0062-unique-paths/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(int x,int y,vector<vector<int>> &dp){
+        if(x==0 && y==0) return 1;
+        if(dp[x][y]!=-1) return dp[x][y];
+        int down=0,right=0;
+        if(x-1>=0)
+            down=solve(x-1,y,dp);
+        if(y-1>=0)
+            right=solve(x,y-1,dp);
+        return dp[x][y]=down+right;
+    }
+    int uniquePaths(int m, int n) {
+      vector<vector<int>> dp(m,vector<int>(n,-1)); 
+      return solve(m-1,n-1,dp); 
+    }
+};
