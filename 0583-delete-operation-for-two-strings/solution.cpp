8cdea231844e29509c9d36248922b05ce625cfdf
Manthan Jain <manthanj1810@gmail.com>
2026-03-06 17:34:43 +0000

    Sync LeetCode submission Runtime - 10 ms (68.81%), Memory - 16.9 MB (21.69%)

---
+++ 0583-delete-operation-for-two-strings/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+       int n=word1.size(),m=size(word2);
+       vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+       for(int i=0;i<=n;i++) dp[i][0]=0;
+       for(int i=0;i<=m;i++) dp[0][i]=0;
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(word1[i-1]==word2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+        }
+       }
+       return m+n-2*dp[n][m];
+    }
+};
