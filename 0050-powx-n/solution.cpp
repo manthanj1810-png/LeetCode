ca586524ca57b11a61ff357f6307c1601f55ea04
Manthan Jain <manthanj1810@gmail.com>
2026-03-08 19:58:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (43.26%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    double revpow(double x, int n){
+    double power(double x,int n){
         if(n==0) return 1;
-        double half=revpow(x,n/2);
+        double half=power(x,n/2);
         if(n%2==0) return half*half;
-        return x*half*half;
+        else return half*half*x; 
     }
     double myPow(double x, int n) {
-        long long N=n;
-       if(N<0){
-        return 1/revpow(x,-N);
-       }
-       return revpow(x,N);  
+        long long m=n;
+        if(m<0){
+            x=1/x;
+            m=-m;
+        }
+        return power(x,m);
     }
 };
