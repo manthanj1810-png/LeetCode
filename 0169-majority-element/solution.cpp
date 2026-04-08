cc9b8f8b82bd38af066104aa9dda51c3ce9de605
Manthan Jain <manthanj1810@gmail.com>
2026-04-08 19:51:28 +0000

    Sync LeetCode submission Runtime - 3 ms (28.02%), Memory - 28.2 MB (67.43%)

---
+++ 0169-majority-element/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size(),mid=n/2;
+        sort(nums.begin(),nums.end());
+        return nums[mid];  
+    }
+};
