7cc71aacbef566a3b2ab8eef728216c9e11a8b59
Manthan Jain <manthanj1810@gmail.com>
2026-06-26 12:05:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 34.3 MB (87.67%)

---
+++ 0450-delete-node-in-a-bst/solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+       if(root==NULL) return NULL;
+       if(root->val==key) return helper(root);
+       TreeNode* curr=root;
+       while(root){
+        if(root->val > key){
+            if(root->left && root->left->val==key){
+                root->left=helper(root->left);
+            }
+            else root=root->left;
+        }
+        else{
+            if(root->right && root->right->val==key) root->right=helper(root->right);
+            else root=root->right;
+        }
+       }
+       return curr;
+    }
+    TreeNode* helper(TreeNode* root){
+        if(root->left==NULL) return root->right;
+        if(root->right==NULL) return root->left;
+        TreeNode *lastr=get(root->left);
+        lastr->right=root->right;
+        return root->left; 
+    }
+    TreeNode* get(TreeNode *root){
+        if(root->right==NULL) return root;
+        return get(root->right);
+    }
+};
