b955e11dc9bfcf2ccfa7e459cfbc26c511412ade
Manthan Jain <manthanj1810@gmail.com>
2026-07-06 10:01:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.7 MB (10.49%)

--- 0073-set-matrix-zeroes/solution.cpp
+++ 0073-set-matrix-zeroes/solution.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
+void set(int r,int c,vector<vector<int>>& matrix){
+    for(int i=0;i<matrix.size();i++) matrix[i][c]=0;
+    for(int i=0;i<matrix[0].size();i++) matrix[r][i]=0;
+}
+bool check(int r,int c,vector<int> &row,vector<int> &col){
+    if(row[r] && col[c]) return true;
+    return false;
+}
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size(),n=matrix[0].size();
-        vector<pair<int,int>> p;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    p.push_back({i,j});
-                }
-            }
+        int n=matrix.size(),m=matrix[0].size();
+       queue<pair<int,int>> q;
+       vector<int> visr(n,0), visc(m,0);
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]==0) q.push({i,j});
         }
-        for(auto &it:p){
-            int i=it.first,j=it.second;
-            for(int a=0;a<n;a++){
-                matrix[i][a]=0;
-            }
-            for(int b=0;b<m;b++){
-                matrix[b][j]=0;
-            }
+       }
+    while(!q.empty()){
+        auto [r,c]=q.front();
+        q.pop();
+        if(!check(r,c,visr,visc)){
+            set(r,c,matrix);
+            visr[r]=1;
+            visc[c]=1;
         }
+    }
     }
 };
