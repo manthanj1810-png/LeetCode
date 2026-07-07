e3fd68ebfcfe814de9ca67eeb72b6b4f3e95025d
Manthan Jain <manthanj1810@gmail.com>
2026-07-07 17:55:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (26.68%)

---
+++ 0238-product-of-array-except-self/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+      vector<int> p(n,1),s(n,1);
+      for(int i=1;i<n;i++){
+        p[i]=p[i-1]*nums[i-1];
+      }
+      for(int i=n-2;i>=0;i--){
+        s[i]=s[i+1]*nums[i+1];
+      }
+      vector<int> ans(n);
+        for(int i=0;i<n;i++){
+            ans[i]=p[i]*s[i];
+        }
+      
+      return ans;  
+    }
+};
